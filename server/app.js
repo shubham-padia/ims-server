2ab21910d72061d97f797600e22d9cbd5401df8e
shubham-padia <shubhamapadia@gmail.com>
2016-08-21 02:50:52 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('express');
+var app = express();
+var config = require('./config/config');
+
+app.listen( config.port, function(){
+    console.log(" app has started listening on " + config.port );
+});
