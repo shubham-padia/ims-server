b83c54c4b1481fe489fa96b8f115f1809076641d
shubham-padia <shubhamapadia@gmail.com>
2016-08-24 03:58:56 +0000

    added upload restrictions for image upload along with error handling
    also sending deatails of the file back in the response

--- server/api/upload/index.js
+++ server/api/upload/index.js
@@ -1,11 +1,31 @@
-var express = require('express')
-var multer  = require('multer')
-var upload = multer({ dest: 'server/public/uploads/' });
+var express = require('express');
+var multer  = require('multer');
+var upload = multer({ 
+    dest: 'server/public/uploads/',
+    limits: {
+        fileSize: 1024 * 3072
+    },
+    rename: function(fieldname, filename) {
+        return filename;
+    },
+    onFileUploadStart: function(file) {
+        console.log('uploading the specified file');
+        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+            console.log("enter a valid image file");
+            return false;
+        }
+    }});
+
 var router = express.Router();
 module.exports = router;
 
-router.post('/upload',upload.single('fileUpload'),function (req, res, next) {
-    console.log(req.file);
-    res.status(204).end();
+router.post('/upload',function (req, res) {
+    upload.single('fileUpload')(req, res, function (err) {
+        if(err){
+            return res.end('{"status": "fail"}');
+        }
+        res.end(req.file);
+    });
+    
 });
 
