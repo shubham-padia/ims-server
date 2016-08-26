d06160f2c42627034cb69e03d51172be7e89b7b2
shubham-padia <shubhamapadia@gmail.com>
2016-08-26 05:16:21 +0000

    disabling voting system for private images

--- api/vote/getVote.js
+++ api/vote/getVote.js
@@ -2,22 +2,28 @@ var express = require('express');
 var router = express.Router();
 module.exports = router;
 var Vote = require('../../schemas/vote');
+var UserImg = require('../../schemas/userImg');
 
 //authentication
 var authCheck = function (req, res, next) {
-	if (req.isAuthenticated())
-		return next();
-    res.json({access: "unauthorized"});    
+    if (req.isAuthenticated())
+        return next();
+    res.json({ access: "unauthorized" });
 };
 //
 
-router.get('/:imgName',authCheck,function(req,res){
-    Vote.find({ imgName: req.params.imgName },function(err, imgVote){
-        var voteCount = 0;
-        imgVote.forEach(function(imgInfo){
-            console.log(imgInfo.vote);
-            voteCount += imgInfo.vote;
-        })
-        res.json({totalVote: voteCount});
-    });
+router.get('/:imgName', authCheck, function (req, res) {
+    UserImg.findOne({ filename: req.params.imgName }, function (err, img) {
+        if (img.privacy != 0 && img.privacy != 1) {
+            res.status(401).send('Voting system is disabled for private images');
+        } else {
+            Vote.find({ imgName: req.params.imgName }, function (err, imgVote) {
+                var voteCount = 0;
+                imgVote.forEach(function (imgInfo) {
+                    voteCount += imgInfo.vote;
+                })
+                res.json({ totalVote: voteCount });
+            })
+        };
+    })
 });
--- api/vote/index.js
+++ api/vote/index.js
@@ -4,74 +4,87 @@ var express = require('express');
 var router = express.Router();
 module.exports = router;
 var Vote = require('../../schemas/vote');
+var UserImg = require('../../schemas/userImg');
 
 
 //authentication
 var authCheck = function (req, res, next) {
-	if (req.isAuthenticated())
-		return next();
-    res.json({access: "unauthorized"});    
+    if (req.isAuthenticated())
+        return next();
+    res.json({ access: "unauthorized" });
 };
 //
 
-router.post('/upvote/:imgName',authCheck,function(req, res , next){
-   Vote.find({userId : req.user.id, imgName : req.params.imgName},function(err,upv){
-       if(err){
-           return res.json({success: false});
-       }
-       if(upv.length === 0){
-            var newVote = Vote();
-            newVote.imgName = req.params.imgName;
-            newVote.userId = req.user.id;
-            newVote.vote = 1;
-
-            newVote.save(function(err){
-                if(err){
-                    return res.json({success: false});
+router.post('/upvote/:imgName', authCheck, function (req, res, next) {
+    UserImg.findOne({ filename: req.params.imgName }, function (err, img) {
+        if (img.privacy != 0 && img.privacy != 1) {
+            res.status(401).send('Voting system is disabled for private images');
+        } else {
+            Vote.find({ userId: req.user.id, imgName: req.params.imgName }, function (err, upv) {
+                if (err) {
+                    return res.json({ success: false });
                 }
-                res.json({success: true});
+                if (upv.length === 0) {
+                    var newVote = Vote();
+                    newVote.imgName = req.params.imgName;
+                    newVote.userId = req.user.id;
+                    newVote.vote = 1;
+
+                    newVote.save(function (err) {
+                        if (err) {
+                            return res.json({ success: false });
+                        }
+                        res.json({ success: true });
+                    });
+                } else {
+                    var newVote = upv[0];
+                    newVote.vote = 1;
+
+                    newVote.save(function (err) {
+                        if (err) {
+                            return res.json({ success: false });
+                        }
+                        res.json({ success: true });
+                    })
+                };
             });
-       } else {
-            var newVote = upv[0];
-            newVote.vote = 1;
-
-            newVote.save(function(err){
-                if(err){
-                    return res.json({success: false});
-                }
-                res.json({success: true});
-            })
-      };
+        }
     });
 });
 
 //downvote
-router.post('/downvote/:imgName',authCheck,function(req, res , next){
-   Vote.find({userId : req.user.id, imgName : req.params.imgName},function(err,upv){
-       if(err){
-           return res.json({success: false});
-       }
-       if(upv.length === 0){
-            var newVote = Vote();
-            newVote.imgName = req.params.imgName;
-            newVote.userId = req.user.id;
-            newVote.vote = -1;
+router.post('/downvote/:imgName', authCheck, function (req, res, next) {
+    UserImg.findOne({ filename: req.params.imgName }, function (err, img) {
+        if (img.privacy != 0 && img.privacy != 1) {
+            res.status(401).send('Voting system is disabled for private images');
+        } else {
+            Vote.find({ userId: req.user.id, imgName: req.params.imgName }, function (err, upv) {
+                if (err) {
+                    return res.json({ success: false });
+                }
+                if (upv.length === 0) {
+                    var newVote = Vote();
+                    newVote.imgName = req.params.imgName;
+                    newVote.userId = req.user.id;
+                    newVote.vote = -1;
 
-            newVote.save(function(err){
-                if(err){
-                    return res.json({success: false});
-                }
-                res.json({success: true});
-            });
-       } else {
-            var newVote = upv[0];
-            newVote.vote = -1;
-            newVote.save(function(err){
-                if(err){
-                    return res.json({success: false});
-                }
-                res.json({success: true});
+                    newVote.save(function (err) {
+                        if (err) {
+                            return res.json({ success: false });
+                        }
+                        res.json({ success: true });
+                    });
+                } else {
+                    var newVote = upv[0];
+                    newVote.vote = -1;
+                    newVote.save(function (err) {
+                        if (err) {
+                            return res.json({ success: false });
+                        }
+                        res.json({ success: true });
+                    })
+                };
             })
-      };
-    });
+        };
+    })
 });
